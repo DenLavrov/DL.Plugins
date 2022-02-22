df5e09bfc6109ca389441bd36043468e5a24353f
DenLavrov <denis.lavrovv@gmail.com>
2022-02-22 12:49:12 +0300

    Fix IsNotEmptyValidationRule

--- Validation/Implementations/IsNotEmptyValidationRule.cs
+++ Validation/Implementations/IsNotEmptyValidationRule.cs
@@ -5,10 +5,12 @@ namespace Validation.Implementations
     public class IsNotEmptyValidationRule: IValidationRule<string>
     {
         public string Message { get; set; }
-        
+
         public ValidationResult Validate(string value)
         {
-            return !string.IsNullOrWhiteSpace(value) ? ValidationResult.Valid() : ValidationResult.Invalid(Message);
+            return !string.IsNullOrEmpty(value) && !string.IsNullOrWhiteSpace(value)
+                ? ValidationResult.Valid()
+                : ValidationResult.Invalid(Message);
         }
     }
 }
